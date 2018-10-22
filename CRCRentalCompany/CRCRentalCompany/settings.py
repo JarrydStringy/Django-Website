71f42050406f8bb0a073b41fc9b02575dc3f3a57
Jarryd Stringfellow <jarryd.stringfellow@gmail.com>
2018-10-22 20:16:36 +1000

    Started to add Postgres db and played with static for html

--- CRCRentalCompany/CRCRentalCompany/settings.py
+++ CRCRentalCompany/CRCRentalCompany/settings.py
@@ -84,15 +84,16 @@ WSGI_APPLICATION = 'CRCRentalCompany.wsgi.application'
 DATABASES = {
     'default': {
         # This is for a sqlite database
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
         # This is for a postgreSQL database
-        # django.db.backends.postgresql_psycopg2
-        # 'NAME': os.environ.get('DB_NAME', ''),
-        # 'USER': os.environ.get('DB_USER', ''),
-        # 'PASSWORD': os.environ.get('DB_PASS', ''),
-        # 'HOST': 'localhost',
-        # 'PORT': '5432',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crc',
+        'USER': 'SuperSonicAdmin',
+        'PASSWORD': 'SuperSonic',
+        'HOST': 'crc.cfoisvv5pzjt.ap-southeast-2.rds.amazonaws.com',    # Localhost
+        'PORT': '5432',
     }
 }
 

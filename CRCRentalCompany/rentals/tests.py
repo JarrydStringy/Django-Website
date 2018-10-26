de59759d504f276b285d3cb31d5c3c5e57cbcf5b
Jarryd Stringfellow <jarryd.stringfellow@gmail.com>
2018-10-26 19:35:06 +1000

    Final Upload for submission.

--- CRCRentalCompany/rentals/tests.py
+++ CRCRentalCompany/rentals/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
+from .models import Order
+from .models import Store
+from .models import Customer
+from .models import Vehicle
+
+class Order (TestCase):
+    def setup(self):
+        Order.objects.create(name = '')
+
+    def test_order_id(self):
+        order = Order.objects.get(id=id)
+        self.assetEqual()
 # Create your tests here.
